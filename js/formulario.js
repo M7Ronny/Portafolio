f9fdebe8597dac8e148627639cc6f27efd195711
M7Ronny <88813726+M7Ronny@users.noreply.github.com>
2021-08-12 17:08:10 -0400

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,77 @@
+const faders = document.querySelectorAll('.fade-in');
+
+$(document).ready(function(){
+
+    $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
+const appearOptions = {
+    threshold: 1
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+    ) {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting){
+            return;
+        } else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);        
+        }
+    });
+},
+appearOptions);
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader);
+});
+    // body...
---
+++ js/main2.js
@@ -0,0 +1,24 @@
+
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
+
+window.addEventListener('load', () => {
+
+const enlaces = document.querySelectorAll('#categorias a');
+
+enlaces.forEach( (elemento) => {
+	console.log(elemento);
+	elemento.addEventListener('click', (evento) => {
+			evento.preventDefault();
+			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+			evento.target.classList.add('activo');
+
+
+			const categoria = evento.target.innerHTML.toLowerCase();
+			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+	    });
+	});
+});
