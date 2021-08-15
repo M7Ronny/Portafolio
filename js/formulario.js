b109f53e98aea59a7e18b80842396da86bbbc541
M7Ronny <88813726+M7Ronny@users.noreply.github.com>
2021-08-14 23:34:19 -0400

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,77 +1,11 @@
-const faders = document.querySelectorAll('.fade-in');
+const $form = document.querySelector('#form')
+const $botonMailto = document.querySelector('#trucazo')
 
-$(document).ready(function(){
+$form.addEventListener('submit', handleSubmit)
 
-    $('#btnSend').click(function(){
-
-        var errores = '';
-
-        // Validado Nombre ==============================
-        if( $('#names').val() == '' ){
-            errores += '<p>Escriba un nombre</p>';
-            $('#names').css("border-bottom-color", "#F14B4B")
-        } else{
-            $('#names').css("border-bottom-color", "#d1d1d1")
-        }
-
-        // Validado Correo ==============================
-        if( $('#email').val() == '' ){
-            errores += '<p>Ingrese un correo</p>';
-            $('#email').css("border-bottom-color", "#F14B4B")
-        } else{
-            $('#email').css("border-bottom-color", "#d1d1d1")
-        }
-
-        // Validado Mensaje ==============================
-        if( $('#mensaje').val() == '' ){
-            errores += '<p>Escriba un mensaje</p>';
-            $('#mensaje').css("border-bottom-color", "#F14B4B")
-        } else{
-            $('#mensaje').css("border-bottom-color", "#d1d1d1")
-        }
-
-        // ENVIANDO MENSAJE ============================
-        if( errores == '' == false){
-            var mensajeModal = '<div class="modal_wrap">'+
-                                    '<div class="mensaje_modal">'+
-                                        '<h3>Errores encontrados</h3>'+
-                                        errores+
-                                        '<span id="btnClose">Cerrar</span>'+
-                                    '</div>'+
-                                '</div>'
-
-            $('body').append(mensajeModal);
-        }
-
-        // CERRANDO MODAL ==============================
-        $('#btnClose').click(function(){
-            $('.modal_wrap').remove();
-        });
-    });
-
-});
-const appearOptions = {
-    threshold: 1
-    rootMargin: "0px 0px -100px 0px"
-};
-
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries,
-    appearOnScroll
-    ) {
-    entries.forEach(entry =>{
-        if (!entry.isIntersecting){
-            return;
-        } else{
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);        
-        }
-    });
-},
-appearOptions);
-
-faders.forEach(fader =>{
-    appearOnScroll.observe(fader);
-});
-    // body...
+function handleSubmit(){
+    event.preventDefault()
+    const form = new FormData(this)
+    $botonMailto.setAttribute('href',`mailto:rrnnn200@gmail.com?subject=${form.get('name')}${form.get('email')}$body=${form.get('message')}`)
+    $botonMailto.click()
+}
